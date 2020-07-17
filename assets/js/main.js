3055a55caf80cfc65303a64853b8577632acbf21
CPL Maker <zirafa@gmail.com>
2020-07-17 23:24:57 +0000

    Updated 'assets/js/main.js' via CloudCannon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,32 @@
 jQuery(function() {
   // Code goes here
-
-    enquire.register("screen and (max-width:45em)", {
+    
+    // Mobile Menu Toggle
+    $('body').on('click', '.mobile-menu-toggle', function(){
+      $('nav').toggleClass('hide');
+    });
+    
+    // https://github.com/WickyNilliams/enquire.js
+    // Media Query plugin for JS
+    enquire.register("screen and (max-width:768px)", {
     
         // OPTIONAL
         // If supplied, triggered when a media query matches.
         match : function() {
-          $('nav').show().addClass('in');
-          
+          $('nav').addClass('hide');    
         },
     
         // OPTIONAL
         // If supplied, triggered when the media query transitions
         // *from a matched state to an unmatched state*.
         unmatch : function() {
-          $('nav').addClass('out').hide();
+          $('nav').removeClass('hide').show();
         },
     
         // OPTIONAL
         // If supplied, triggered once, when the handler is registered.
         setup : function() {
-          $('nav').hide();        
+          // $('nav').hide();        
         },
     
         // OPTIONAL, defaults to false
