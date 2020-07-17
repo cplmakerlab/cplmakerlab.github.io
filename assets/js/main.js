5f5e0431ee20c9f2cbb0c64e9f96905a13a74ec3
CPL Maker <zirafa@gmail.com>
2020-07-17 23:14:58 +0000

    Updated 'assets/js/main.js' via CloudCannon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
 jQuery(function() {
   // Code goes here
+
+    enquire.register("screen and (max-width:45em)", {
+    
+        // OPTIONAL
+        // If supplied, triggered when a media query matches.
+        match : function() {
+          $('nav').show().addClass('in');
+          
+        },
+    
+        // OPTIONAL
+        // If supplied, triggered when the media query transitions
+        // *from a matched state to an unmatched state*.
+        unmatch : function() {
+          $('nav').addClass('out').hide();
+        },
+    
+        // OPTIONAL
+        // If supplied, triggered once, when the handler is registered.
+        setup : function() {
+          $('nav').hide();        
+        },
+    
+        // OPTIONAL, defaults to false
+        // If set to true, defers execution of the setup function
+        // until the first time the media query is matched
+        deferSetup : true,
+    
+        // OPTIONAL
+        // If supplied, triggered when handler is unregistered.
+        // Place cleanup code here
+        destroy : function() {}
+    
+    });    
+    
 });
+
