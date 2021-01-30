b585ddcdc4de65b2ec169149e3a4d2f05ec50eb5
CPL Maker <makerspace@chipublib.org>
2021-01-30 04:26:33 +0000

    Updated 'assets/js/main.js' via CloudCannon

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ jQuery(function() {
      $('input.js-filter-search').on('keyup', function () {
         var filter = $(this).val().toUpperCase();
         if (filter) {
-          $(".list li").find("a:not(:cicontains(" + filter + "))").parent().hide();
+          $('.list li').find("a:not(:cicontains(" + filter + "))").parent().hide();
           $('.list li').find("a:cicontains(" + filter + ")").parent().show();
         } else {
           $('.list li').show();
